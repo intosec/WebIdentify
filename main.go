f7b1d83f36e62330ec81bac37059a179f06f3369
root <root@localhost.localdomain>
2016-04-25 16:53:47 +0800

    Compress file into zip

--- main.go
+++ main.go
@@ -2,9 +2,9 @@
 package main
 
 import (
-	//"archive/tar"
+	"archive/zip"
 	"bufio"
-	//"compress/gzip"
+	"bytes"
 	"crypto/tls"
 	"encoding/json"
 	"flag"
@@ -13,6 +13,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strings"
 	"sync"
@@ -140,10 +141,64 @@ func Worker(ch chan Target) {
 	wg.Done()
 }
 
+// 参数frm可以是文件或目录，不会给dst添加.zip扩展名
+func Compressor(frm, dst string) error {
+	buf := bytes.NewBuffer(make([]byte, 0, 10*1024*1024)) // 创建一个读写缓冲
+	myzip := zip.NewWriter(buf)                           // 用压缩器包装该缓冲
+	// 用Walk方法来将所有目录下的文件写入zip
+	err := filepath.Walk(frm, func(path string, info os.FileInfo, err error) error {
+		var file []byte
+		if err != nil {
+			return filepath.SkipDir
+		}
+		header, err := zip.FileInfoHeader(info) // 转换为zip格式的文件信息
+		if err != nil {
+			return filepath.SkipDir
+		}
+		header.Name, _ = filepath.Rel(filepath.Dir(frm), path)
+		if !info.IsDir() {
+			// 确定采用的压缩算法（这个是内建注册的deflate）
+			header.Method = 8
+			file, err = ioutil.ReadFile(path) // 获取文件内容
+			if err != nil {
+				return filepath.SkipDir
+			}
+		} else {
+			file = nil
+		}
+		// 上面的部分如果出错都返回filepath.SkipDir
+		// 下面的部分如果出错都直接返回该错误
+		// 目的是尽可能的压缩目录下的文件，同时保证zip文件格式正确
+		w, err := myzip.CreateHeader(header) // 创建一条记录并写入文件信息
+		if err != nil {
+			return err
+		}
+		_, err = w.Write(file) // 非目录文件会写入数据，目录不会写入数据
+		if err != nil {        // 因为目录的内容可能会修改
+			return err // 最关键的是我不知道咋获得目录文件的内容
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	myzip.Close()               // 关闭压缩器，让压缩器缓冲中的数据写入buf
+	file, err := os.Create(dst) // 建立zip文件
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = buf.WriteTo(file) // 将buf中的数据写入文件
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	verbose = flag.Bool("v", false, "Verbose on")
-	timeout = flag.Int("w", 10, "Timeout")
-	//var compress = flag.Bool("c", false, "Compress result file and delete original file")
+	timeout = flag.Int("w", 30, "Timeout")
+	var compress = flag.Bool("c", false, "Compress result file and delete original file")
 	var iplist = flag.String("i", "", "IP list file")
 	var hostlist = flag.String("h", "", "Host list file")
 	var output = flag.String("o", "", "Output file, scan result")
@@ -197,45 +252,10 @@ func main() {
 	wg.Wait()
 	result.Close()
 
-	//	if *compress {
-	//		fw, err := os.Create(*output + ".tar.gz")
-	//		if err != nil {
-	//			fmt.Println(err.Error())
-	//			os.Exit(1)
-	//		}
-	//		defer fw.Close()
-	//		gw, _ := gzip.NewWriterLevel(fw, gzip.BestCompression)
-	//		defer gw.Close()
-	//		// tar write
-	//		tw := tar.NewWriter(gw)
-	//		defer tw.Close()
-	//		fr, err := os.Open(*output)
-	//		if err != nil {
-	//			panic(err)
-	//		}
-	//		defer fr.Close()
-	//		fi, err := fr.Stat()
-	//		if err != nil {
-	//			panic(err)
-	//		}
-	//		h := new(tar.Header)
-	//		h.Name = fr.Name()
-	//		h.Size = fi.Size()
-	//		h.Mode = int64(fi.Mode())
-	//		h.ModTime = fi.ModTime()
-	//		// 写信息头
-	//		err = tw.WriteHeader(h)
-	//		if err != nil {
-	//			panic(err)
-	//		}
-	//		// 写文件
-	//		_, err = io.Copy(tw, fr)
-	//		if err != nil {
-	//			panic(err)
-	//		}
-	//		fr.Close()
-	//		os.Remove(*output)
-	//		fmt.Println("File compressed")
-	//	}
+	if *compress {
+		Compressor(*output, *output+".zip")
+		//os.Remove(*output)
+		fmt.Println("File compressed")
+	}
 	fmt.Println("Done!")
 }
